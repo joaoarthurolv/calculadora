c4fa27568a658da8ca02ce9f32a4850b1adb4f3a
João Arthur <joaoarthur988@gmail.com>
2023-10-03 14:52:32 -0300

    Criação de layout da calculadora numérica

--- app/src/main/java/br/ufrn/imd/calculadora/fragments/NumericFragment.java
+++ app/src/main/java/br/ufrn/imd/calculadora/fragments/NumericFragment.java
@@ -2,11 +2,14 @@ package br.ufrn.imd.calculadora.fragments;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import br.ufrn.imd.calculadora.R;
 
@@ -18,7 +21,25 @@ public class NumericFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeric, container, false);
+        View view = inflater.inflate(R.layout.fragment_numeric, container, false);
+
+        TextView tvResult = view.findViewById(R.id.tvResult);
+
+        ConstraintLayout constraintLayout = view.findViewById(R.id.idConstraintLayoutNumeric);
+
+        for (int i = 0; i < constraintLayout.getChildCount(); i++) {
+            View view1 = constraintLayout.getChildAt(i);
+            if (view1 instanceof Button) {
+                Button button = (Button) view1;
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        tvResult.setText(tvResult.getText().toString() + button.getText());
+                    }
+                });
+            }
+        }
+
+        return view;
     }
 }
